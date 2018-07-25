f4fc97b26de070adde405b733f2bb2e56e46f983
lonelymoon <z454236029@gmail.com>
2018-07-25 19:08:20 +0800

    性能优化测试

--- pages/index.vue
+++ pages/index.vue
@@ -9,22 +9,22 @@
             <gl-title text="热门游戏排行"></gl-title>
             <gl-panels></gl-panels>
           </div>
-          <div class="mb-3" v-if="secondLoad">
+          <div class="mb-3">
             <gl-title text="最新游戏排行"></gl-title>
             <gl-panels></gl-panels>
           </div>
-          <div class="mb-3" v-if="thirdLoad">
+          <div class="mb-3" v-if="false">
             <gl-title>近期手游流行趋势对比图</gl-title>
             <gl-echarts-items></gl-echarts-items>
           </div>
         </v-flex>
         <v-flex xs12 sm12 md6 order-sm1 order-md2 px-2>
-          <div class="mb-3" v-if="secondLoad">
+          <div class="mb-3">
             <gl-slider keyname="home-slide2" type="2" height="160" class="mb-3" :slides="middleSlides"></gl-slider>
             <gl-title text="热门游戏社区"></gl-title>
             <gl-grid-items col="5"></gl-grid-items>
           </div>
-          <div class="mb-3" v-if="thirdLoad">
+          <div class="mb-3">
             <gl-article-list v-for="n in 5" :key="n"></gl-article-list>
           </div>
         </v-flex>
@@ -113,10 +113,7 @@ export default {
       thirdLoad: false
     }
   },
-  async mounted() {
-    await setTimeout(() => { this.secondLoad = true }, 500)
-    await setTimeout(() => { this.thirdLoad = true }, 500)
-  }
+  mounted() {}
 }
 </script>
 
