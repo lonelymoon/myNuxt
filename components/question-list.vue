b44c436d390d590bbbf5f6d191d496a8d8cd3fe7
lonelymoon <z454236029@gmail.com>
2018-07-26 14:36:02 +0800

    添加问答列表组件，测试自动部署

---
+++ components/question-list.vue
@@ -0,0 +1,20 @@
+<template>
+<div class="gl-question-list-wrapper">
+  <v-card>
+    list
+  </v-card>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'gl-question-list'
+}
+</script>
+
+<style scoped>
+.gl-question-list-wrapper{
+  position: relative;
+  height: auto;
+}
+</style>
