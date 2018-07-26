68821fdf9782e62738a25c2ed14132852d18425f
lonelymoon <z454236029@gmail.com>
2018-07-26 14:25:37 +0800

    更新question页面结构，测试自动部署

--- pages/question/index.vue
+++ pages/question/index.vue
@@ -3,10 +3,20 @@
   <v-container fluid px-1>
     <v-layout row wrap>
       <v-flex md8 xs12 px-2>
-        <div class="blue">asd</div>
+        <div class="blue">
+          <gl-title>问答列表展示</gl-title>
+        </div>
       </v-flex>
       <v-flex md4 xs12 px-2>
-        <div class="red">asda</div>
+        <div class="mb-3">
+          <gl-title>热门问答排行</gl-title>
+        </div>
+        <div class="mb-3">
+          <gl-title>用户积分排行</gl-title>
+        </div>
+        <div class="mb-3">
+          <gl-title>社区热度排行</gl-title>
+        </div>
       </v-flex>
     </v-layout>
   </v-container>
@@ -14,9 +24,16 @@
 </template>
 
 <script>
+import {
+  GlTitle
+} from '../../components/'
+
 export default {
   head: {
     title: '问答'
+  },
+  components: {
+    GlTitle
   }
 }
 </script>
