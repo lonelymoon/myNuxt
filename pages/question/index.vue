2f01b38158cd5c6b55a64ae7e5de246c11f76b80
lonelymoon <z454236029@gmail.com>
2018-07-26 12:09:36 +0800

    添加问答页面

---
+++ pages/question/index.vue
@@ -0,0 +1,17 @@
+<template>
+<section id="gl-page">
+  <div>hello,world</div>
+</section>
+</template>
+
+<script>
+export default {
+  head: {
+    title: '问答'
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
