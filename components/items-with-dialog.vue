740d93b546685c4176aedc1e5574d1a2ea16afb7
lonelymoon <z454236029@gmail.com>
2018-07-31 19:06:28 +0800

    1. 添加展示-items标签 2. 更新攻略页面

--- components/items-with-dialog.vue
+++ components/items-with-dialog.vue
@@ -1,15 +1,53 @@
 <template>
 <div class="gl-items-with-dialog-wrapper">
-  sadsa
+  <v-card>
+    <div class="gl-items-with-dialog pa-3">
+      <div class="gl-items-with-dialog-item px-3 mb-3" :style="itemStyles" v-for="n in 10" :key="n">
+        <div class="gl-items-with-dialog-item-wrapper">
+          <div class="gl-items-with-dialog-item-face">
+            <img src="/faces/1.jpg" class="img-block"/>
+            <div class="mt-2">游戏</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </v-card>
 </div>
 </template>
 
 <script>
 export default {
-  name: 'gl-items-with-dialog'
+  name: 'gl-items-with-dialog',
+  computed: {
+    itemStyles() {
+      return {
+        minWidth: '20%',
+        maxWidth: '20%'
+      }
+    }
+  }
 }
 </script>
 
 <style scoped>
+.gl-items-with-dialog-wrapper{
+  position: relative;
+  height: auto;
+}
 
+.gl-items-with-dialog{
+  position: relative;
+  height: auto;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+}
+
+.gl-items-with-dialog-item{
+  position: relative;
+  height: auto;
+  box-sizing: border-box;
+  flex: 1;
+  text-align: center;
+}
 </style>
